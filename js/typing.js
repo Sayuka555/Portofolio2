a20c765a387b6cb3c01a1a27edd47baf8ac70e76
ueta <ueta1312tmjd@gmail.com>
2025-02-10 01:40:23 +0900

    タイピングアニメーションの下書きをコメントアウトで追加

---
+++ js/typing.js
@@ -0,0 +1,12 @@
+// const typing = (el, sentence) => {
+//   [...sentence].forEach((char, index) => {
+//     setTimeout(() => {
+//       document.querySelector(el).textContent += char;
+//     }, 100 * ++index);
+//   });
+// };
+
+// typing(
+//   '.typing-animation',
+//   'ポートフォリオをご覧いただきありがとうございます！'
+// );
